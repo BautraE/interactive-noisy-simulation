ff8db7c0f2ba239074f69c64cc5fc807c38da66d
Edgars Bautra <edgars.bautra12@gmail.com>
2025-08-13 15:04:33 +0300

    BF3 - Obtaining coupling map and basis gates
    Added new functionality for obtaining relevant basis gates based on provided CSV data, as well as the ability to automatically create a coupling map.
    
    Side note: The new method "get_coupling_map" will eventually be somewhere else as it is only used when creating a simulator instance with AerSimulator.

--- noise_creator.py
+++ noise_creator.py
@@ -1,5 +1,6 @@
 import json, pandas
 from noise_data_manager import NoiseDataManager
+from qiskit.transpiler import CouplingMap
 from qiskit_aer.noise import (NoiseModel, ReadoutError, depolarizing_error, thermal_relaxation_error)
 
 with open("data/csv_columns.json", "r") as file:
@@ -8,46 +9,52 @@ with open("data/config.json", "r") as file:
     CONFIG = json.load(file)
 
 class NoiseCreator:
-    def __init__(self):
+    def __init__(self) -> None:
         print(f"----- Creating {NoiseCreator.__name__} object -----")
-        self.__noise_model = NoiseModel()
+        self.__noise_model = None
         self.__noise_data = None
         f"{NoiseCreator.__name__} class object has been created successfully!\n"
 
-    def link_noise_data_manager(self, noise_data_manager: NoiseDataManager):
+    def link_noise_data_manager(self, noise_data_manager: NoiseDataManager) -> None:
         self.__noise_data = noise_data_manager.noise_data
 
-    def create_noise_model(self):
+    def create_noise_model(self) -> None:
         if self.__noise_data is None:
             raise RuntimeError("There is no linked NoiseDataManager object! Please use the method link_noise_data_manager(ndm: NoiseDataManager) before creating a noise model!")
+        
+        self.__noise_model = NoiseModel(self.__get_basis_gates())
+
         for qubit_nr, columns in self.__noise_data.iterrows():
             self.__add_readout_error(qubit_nr, columns)
             self.__add_depolarizing_error(qubit_nr, columns)
             self.__add_thermal_error(qubit_nr, columns)
     
-    def __add_readout_error(self, qubit_nr, columns):
+    def __add_readout_error(self, qubit_nr, columns) -> None:
         m0p1 = columns[CSV_COLUMNS["m0p1"]["csv_name"]]
         m1p0 = columns[CSV_COLUMNS["m0p1"]["csv_name"]]
         readout_error = ReadoutError([[1-m0p1, m0p1], [m1p0, 1-m1p0]])
         self.__noise_model.add_readout_error(readout_error, [qubit_nr])
 
-    def __add_depolarizing_error(self, qubit_nr, columns):
+    def __add_depolarizing_error(self, qubit_nr, columns) -> None:
         for instruction in CONFIG["single_qubit_gates"]:
-            error_data = columns[CSV_COLUMNS[instruction]["csv_name"]]
-            # In one case the single-qubit gate error values were NaN for one qubit.
-            # Not sure if this was a bug on their side, but this validation fixes this issue.
-            if not pandas.isna(error_data):
-                error = depolarizing_error(error_data, 1)
-                self.__noise_model.add_quantum_error(error, CSV_COLUMNS[instruction]["code_name"], [qubit_nr])
+            if CSV_COLUMNS[instruction]["csv_name"] in self.__noise_data.columns:
+                error_data = columns[CSV_COLUMNS[instruction]["csv_name"]]
+                # In one case the single-qubit gate error values were NaN for one qubit.
+                # Not sure if this was a bug on their side, but this validation fixes this issue.
+                if not pandas.isna(error_data):
+                    error = depolarizing_error(error_data, 1)
+                    self.__noise_model.add_quantum_error(error, CSV_COLUMNS[instruction]["code_name"], [qubit_nr])
 
         for instruction in CONFIG["two_qubit_gates"]:
-            error_data = columns[CSV_COLUMNS[instruction]["csv_name"]]
-            if not pandas.isna(error_data):
-                for target_qubit in error_data.keys():
-                    error = depolarizing_error(error_data[target_qubit], 2)
-                    self.__noise_model.add_quantum_error(error, CSV_COLUMNS[instruction]["code_name"], [qubit_nr, target_qubit])
+            if CSV_COLUMNS[instruction]["csv_name"] in self.__noise_data.columns:
+                error_data = columns[CSV_COLUMNS[instruction]["csv_name"]]
+                if not pandas.isna(error_data):
+                    # Since each row of these columns may contain more than one data entry
+                    for target_qubit in error_data.keys():
+                        error = depolarizing_error(error_data[target_qubit], 2)
+                        self.__noise_model.add_quantum_error(error, CSV_COLUMNS[instruction]["code_name"], [qubit_nr, target_qubit])
 
-    def __add_thermal_error(self, qubit_nr, columns):
+    def __add_thermal_error(self, qubit_nr, columns) -> None:
         # Since base value is seconds, converting both to microseconds
         t1_time = columns[CSV_COLUMNS["t1_time"]["csv_name"]] * 1e-6
         # Some T2 values are bigger than 2*T1 so they need to be truncated
@@ -57,7 +64,8 @@ class NoiseCreator:
         # Also converting all gate time values to nanoseconds since base value is in seconds
         thermal_error_1q = thermal_relaxation_error(t1_time, t2_time, columns[CSV_COLUMNS["1q_gate_time"]["csv_name"]] * 1e-9)
         for instruction in CONFIG["single_qubit_gates"]:
-            self.__noise_model.add_quantum_error(thermal_error_1q, CSV_COLUMNS[instruction]["code_name"], [qubit_nr], warnings=False)
+            if CSV_COLUMNS[instruction]["csv_name"] in self.__noise_data.columns:
+                self.__noise_model.add_quantum_error(thermal_error_1q, CSV_COLUMNS[instruction]["code_name"], [qubit_nr], warnings=False)
 
         # Similarly, all two-qubit gates have the same thermal relaxation error
         two_qubit_gate_time = columns[CSV_COLUMNS["2q_gate_time"]["csv_name"]]
@@ -70,7 +78,8 @@ class NoiseCreator:
                 thermal_err_2q = thermal_relaxation_error(t1_time, t2_time, two_qubit_gate_time[target_qubit] * 1e-9).expand(
                         thermal_relaxation_error(t1_time_q2, t2_time_q2, two_qubit_gate_time[target_qubit] * 1e-9))
                 for instruction in CONFIG["two_qubit_gates"]:
-                     self.__noise_model.add_quantum_error(thermal_err_2q, CSV_COLUMNS[instruction]["code_name"], [qubit_nr, target_qubit], warnings=False)
+                    if CSV_COLUMNS[instruction]["csv_name"] in self.__noise_data.columns:
+                        self.__noise_model.add_quantum_error(thermal_err_2q, CSV_COLUMNS[instruction]["code_name"], [qubit_nr, target_qubit], warnings=False)
 
         # Creating and adding thermal relax error for measure operation
         readout_time = columns[CSV_COLUMNS["readout_time"]["csv_name"]] * 1e-9
@@ -81,3 +90,26 @@ class NoiseCreator:
         reset_time = columns[CSV_COLUMNS["reset_time"]["csv_name"]] * 1e-9
         thermal_err_reset = thermal_relaxation_error(t1_time, t2_time, reset_time)
         self.__noise_model.add_quantum_error(thermal_err_reset, "reset", [qubit_nr])
+
+    def __get_basis_gates(self) -> list[str]:
+        basis_gate_list = CONFIG["non_gate_instructions"]
+
+        for instruction in CONFIG["single_qubit_gates"]:
+            if CSV_COLUMNS[instruction]["csv_name"] in self.__noise_data.columns:
+                basis_gate_list.append(CSV_COLUMNS[instruction]["code_name"])
+        
+        for instruction in CONFIG["two_qubit_gates"]:
+            if CSV_COLUMNS[instruction]["csv_name"] in self.__noise_data.columns:
+                basis_gate_list.append(CSV_COLUMNS[instruction]["code_name"])
+
+        return basis_gate_list
+    
+    def get_coupling_map(self) -> CouplingMap:
+        coupled_qubits = []
+        for qubit, columns in self.__noise_data.iterrows():
+            neighboring_qubits = columns[CSV_COLUMNS["neighboring_qubits"]["csv_name"]]
+            if isinstance(neighboring_qubits, list):
+                for paired_qubit in neighboring_qubits:
+                    coupled_qubits.append([qubit, paired_qubit])
+
+        return CouplingMap(couplinglist=coupled_qubits)
--- noise_data_manager.py
+++ noise_data_manager.py
@@ -1,4 +1,4 @@
-import pandas, json
+import pandas, json, numpy
 from typing import Union
 
 with open("data/csv_columns.json", "r") as file:
@@ -9,7 +9,7 @@ with open("data/config.json", "r") as file:
     CONFIG = json.load(file)
 
 class NoiseDataManager:
-    def __init__(self):
+    def __init__(self) -> None:
         print(f"\033[1m----- Creating {NoiseDataManager.__name__} object -----\033[0m")
         self.__dataframe = None
         print(
@@ -22,25 +22,39 @@ class NoiseDataManager:
         return self.__dataframe
 
     # For importing calibration data from IBM Quantum QPU's
-    def import_csv_data(self, file_path: str):
+    def import_csv_data(self, file_path: str) -> None:
         print(f"----- Importing data from the selected CSV file -----")
         self.__dataframe = pandas.read_csv(file_path)
         
         # Removing unnecessary collumns from the table (not used for creating NoiseModel)
         for column in CONFIG["not_required_columns"]:
-            self.__dataframe.pop(column)
+            if column in self.__dataframe.columns:
+                self.__dataframe.pop(column)
         
-        # Modifying multi-value columns to a better format
+        # Initializing new column
+        column_neighboring_qubits = CSV_COLUMNS["neighboring_qubits"]["csv_name"]
+        self.__dataframe[column_neighboring_qubits] = numpy.nan
+        self.__dataframe[column_neighboring_qubits].astype(object)
+
         for qubit in range(len(self.__dataframe)):
-             for attribute_key in CONFIG["multi_data_columns"]:
+            neighboring_qubits = []
+            NEIGHBORS_FOUND = False
+            for attribute_key in CONFIG["multi_data_columns"]:
                 attribute_name = CSV_COLUMNS[attribute_key]["csv_name"]
+                # If the attribute value column is in the table and it's value is not a NaN
                 if attribute_name in self.__dataframe.columns and isinstance(self.__dataframe.at[qubit, attribute_name], str):
                     attribute_value_list = self.__dataframe.at[qubit, attribute_name].split(";")
                     modified_data = {}
+                    # Modifying multi-value columns to a better format
                     for attribute_value in attribute_value_list:
                         target_qubit, value = attribute_value.split(":")
                         modified_data[int(target_qubit)] = float(value)
+                        # Adding neighboring qubit to list
+                        if not NEIGHBORS_FOUND:
+                            neighboring_qubits.append(int(target_qubit))
                     self.__dataframe.at[qubit, attribute_name] = modified_data
+                    self.__dataframe.at[qubit, column_neighboring_qubits] = neighboring_qubits
+                    NEIGHBORS_FOUND = True
 
         # This might change, if they add this information in the CSV files at some point in time
         print(f"CSV files currently do not contain Reset operation times. A default value of \033[32m1300 ns\033[0m has been set.")
@@ -48,7 +62,7 @@ class NoiseDataManager:
         print(f"Data from CSV file has been imported successfully!\n")
 
     # For requesting lookup of certain qubit calibration data
-    def get_qubit_noise_information(self, qubits: Union[int, list[int]]):
+    def get_qubit_noise_information(self, qubits: Union[int, list[int]]) -> None:
         # So that individual integers are processed just like lists
         if isinstance(qubits, int):
             qubits = [qubits]
@@ -66,7 +80,7 @@ class NoiseDataManager:
             print(f"\n")
 
     # Explains all CSV calibration data columns to the user
-    def help_csv_columns(self):
+    def help_csv_columns(self) -> None:
         print(f"----- Information on all currently relevant CSV calibration data columns -----")
         for column in CSV_COLUMNS.keys():
             print(f"\033[32m{CSV_COLUMNS[column]["name"]}:\033[0m {CSV_COLUMNS[column]["description"]}")
