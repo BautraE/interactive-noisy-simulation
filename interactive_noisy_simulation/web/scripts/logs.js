265c66648d59548e21f3761973951ffd408d4639
Edgars Bautra <edgars.bautra12@gmail.com>
2026-01-22 13:57:55 +0200

    AF3_6 - Added styling for message & error log
    Mostly just styling related changes to the message & error log container which now is hidden by default and can be made visible by clicking a button.

--- interactive_noisy_simulation/web/scripts/logs.js
+++ interactive_noisy_simulation/web/scripts/logs.js
@@ -1,8 +1,28 @@
+// =========================================================
+// Functionality related to message and error log
+// =========================================================
+
+// Loading existing messages into log content box
 window.addEventListener('load', function() {
     setTimeout(function() {
         eel.show_log_messages();
     }, 1);
 });
+
+
+// Showing / hiding log sidebar
+function showLogSidebar() {
+    let sidebar = document.getElementById("log-sidebar");
+    sidebar.classList.remove('hidden-log');
+    sidebar.classList.add('visible-log');
+}
+
+
+function hideLogSidebar() {
+    let sidebar = document.getElementById("log-sidebar");
+    sidebar.classList.remove('visible-log');
+    sidebar.classList.add('hidden-log');
+}
 
 
 eel.expose(loadLogMessage)
@@ -11,24 +31,29 @@ function loadLogMessage(id, timestamp, messageText) {
     let aDelete = document.createElement("a");
     aDelete.innerHTML = "Clear";
     aDelete.role = "button";
-    aDelete.onclick = () => eel.clear_message(id)
+    aDelete.classList.add("log-clear-action");
+    aDelete.onclick = () => eel.clear_message(id);
 
     let deleteContainer = document.createElement("div");
+    deleteContainer.classList.add("log-instance-action-box");
     deleteContainer.appendChild(aDelete);
 
     // Message part with timestamp and text
     let pTimestamp = document.createElement("p");
+    pTimestamp.classList.add("log-timestamp");
     pTimestamp.innerHTML = timestamp;
     let pMessage = document.createElement("p");
     pMessage.innerHTML = messageText;
 
     let messageContentContainer = document.createElement("div");
+    messageContentContainer.classList.add("log-instance-content-box");
     messageContentContainer.appendChild(pTimestamp);
     messageContentContainer.appendChild(pMessage);
 
     // Combining both containers into one for the entire message
     // instance
     let messageContainer = document.createElement("div");
+    messageContainer.classList.add("log-instance");
     messageContainer.appendChild(deleteContainer);
     messageContainer.appendChild(messageContentContainer);
 
